51661b781efea96a496d685a15609655c293a5b4
Sergio <santossergiorealbjj@outlook.com>
2025-11-28 19:06:42 -0300

    Create GenAiEtlApplication.java

---
+++ api/src/main/java/com/santander/genai/etl/GenAiEtlApplication.java
@@ -0,0 +1,11 @@
+package com.santander.genai.etl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenAiEtlApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GenAiEtlApplication.class, args);
+    }
+}
