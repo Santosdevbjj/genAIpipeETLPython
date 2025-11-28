d9350285f966db799e288dc88ebf97adf09af2b7
Sergio <santossergiorealbjj@outlook.com>
2025-11-28 20:42:04 -0300

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+# docs/conf.py
+project = 'GenAI ETL - Santander'
+author = 'Equipe'
+release = '0.1.0'
+
+extensions = ['myst_parser']
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
