63e69cd2d1d35d2f938c7d44884925ac9057d2c1
Sergio <santossergiorealbjj@outlook.com>
2025-11-28 19:10:08 -0300

    Create OpenApiConfig.java

---
+++ api/src/main/java/com/santander/genai/etl/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.santander.genai.etl.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.*;
+import io.swagger.v3.oas.models.security.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI apiDocs() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("GenAI ETL API - Santander")
+                        .version("0.1.0")
+                        .description("API RESTful para clientes e mensagens de marketing com segurança OAuth2/JWT"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
